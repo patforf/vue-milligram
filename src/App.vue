7f7a47722ee939f1cc9785af24c1222be2b640eb
pathé <patforf@gmail.com>
2020-01-09 16:34:17 +0100

    button

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,29 @@
       <VmBlockquote text="Mon text">mmmmmm</VmBlockquote>
       <VmBlockquote text="Mon autre text"></VmBlockquote>
       <VmHeading v-for="(h, index) in num" :key="index"  :type="h" :text="`Mon titre  ${h}`"></VmHeading>
+      <VmButton texte="Mon bouton"></VmButton>
+      <VmButton texte="Mon bouton" outline></VmButton>
+      <VmButton texte="Mon bouton" clear @click="cliqueSurBouton"></VmButton>
+        
   </div>
 </template>
 
 <script>
 import VmBlockquote from './components/VmBlockquote' 
 import VmHeading from './components/VmHeading'
+import VmButton from './components/VmButton'
 export default {
   name: 'app',
   components:{
     VmBlockquote,
     VmHeading,
-   
+    VmButton,
+  
+  },
+  method:{
+    cliqueSurBouton(){
+        console.log('click sur enfant');
+    }
   },
   data(){
     return{
---
+++ src/components/VmButton.vue
@@ -0,0 +1,25 @@
+<template>
+  
+   <a :class="{ 'button-outline':outline, 'button-clear':clear }"  class="button" href="#" @click="clickBtn">Default Button</a>
+  
+</template>
+
+<script>
+export default {
+   name: 'VmButton',
+   props: {
+   outline:Boolean,
+   clear:Boolean
+    },
+    method:{
+       clickBtn(evenement){
+          this.$emit('click',evenement);
+       }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
